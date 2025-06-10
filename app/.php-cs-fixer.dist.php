9a391c0a4c64535464c341d554845eb01b102be9
Scimbor <marcin.scim@gmail.com>
2025-06-10 21:45:36 +0200

    Init project files

---
+++ app/.php-cs-fixer.dist.php
@@ -0,0 +1,21 @@
+<?php
+
+$finder = PhpCsFixer\Finder::create()
+    ->in(__DIR__)
+    ->exclude('vendor')
+    ->exclude('storage')
+    ->exclude('bootstrap/cache');
+
+return (new PhpCsFixer\Config())->setRiskyAllowed(true)->setRules([
+        '@PSR12' => true,
+        'array_syntax' => ['syntax' => 'short'],
+        'binary_operator_spaces' => ['default' => 'single_space'],
+        'blank_line_after_namespace' => true,
+        'blank_line_after_opening_tag' => true,
+        'cast_spaces' => ['space' => 'single'],
+        'class_attributes_separation' => ['elements' => ['method' => 'one']],
+        'no_unused_imports' => true,
+        'ordered_imports' => ['sort_algorithm' => 'alpha'],
+        'single_quote' => true,
+        'trailing_comma_in_multiline' => ['elements' => ['arrays']],
+    ])->setFinder($finder);
---
+++ start.sh
@@ -0,0 +1,39 @@
+#!/bin/bash
+
+echo "Generating SSL certificates"
+
+openssl genpkey -algorithm RSA -out /etc/ssl/private/nginx-selfsigned.key
+openssl req -new -key /etc/ssl/private/nginx-selfsigned.key -out /etc/ssl/private/nginx-selfsigned.csr \
+    -subj "/C=CA/ST=QC/O=Company Inc/CN=example.com"
+openssl x509 -req -days 365 -in /etc/ssl/private/nginx-selfsigned.csr -signkey /etc/ssl/private/nginx-selfsigned.key -out /etc/ssl/certs/nginx-selfsigned.crt
+
+echo "Create directories in storage"
+
+mkdir -p "storage"
+mkdir -p "bootstrap/cache"
+
+chmod -R 777 "storage" && chmod -R 777 "bootstrap/cache" && chmod -R 777 "packages"
+
+mkdir -p "storage/app"
+mkdir -p "storage/app/public"
+mkdir -p "storage/framework"
+mkdir -p "storage/framework/cache"
+mkdir -p "storage/framework/cache/data"
+mkdir -p "storage/framework/sessions"
+mkdir -p "storage/framework/testing"
+mkdir -p "storage/framework/views"
+mkdir -p "storage/logs"
+mkdir -p "storage/database"
+
+touch storage/database/database.sqlite && chmod -R 777 storage/database/database.sqlite
+
+composer install
+
+cd /var/www/html
+
+php artisan migrate --force
+php artisan optimize
+
+/usr/sbin/service php8.3-fpm start
+/usr/sbin/service nginx start
+tail -f /dev/null
